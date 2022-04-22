1234e5ed530630b0285acebbece581439b489346
aman9saw <sawaman9@gmail.com>
2022-04-22 10:59:24 +0530

    adding the log in test

--- src/test/java/com/jenkin/SpringJenkinsApplicationTests.java
+++ src/test/java/com/jenkin/SpringJenkinsApplicationTests.java
@@ -1,13 +1,20 @@
 package com.jenkin;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
+	static Logger log = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 	@Test
 	void contextLoads() {
+		log.info("Test case executing");
+		assertEquals(true, true);
 	}
 
 }
